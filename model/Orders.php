7eed184a23ad32b510aec903985ff90cee791ffa
Student 9 <user9@tc.geeksforless.net>
2015-04-03 06:54:44 +0300

    Orders model

--- model/Orders.php
+++ model/Orders.php
@@ -9,6 +9,50 @@ class Users
     {
         $this->mysql = MySql::getInstance();
     }
-
+	
+	public function orderAdd($data)
+	{
+		$res = $this->mysql->insert('orders', $data);
+		if($res)
+            return true;
+        else
+            return false;
+	}
+	
+	public function orderDelete($where)
+	{
+		$res = $this->mysql->insert('orders', $where);
+		if($res)
+            return true;
+        else
+            return false;
+	}
+	
+	public function orderUpdate($statusName, $id)
+	{
+		$where = array('id' => $id);
+		$data = array('status_name' => $statusName);
+		$res = $this->mysql->update('orders', $where, $data);
+		if($res)
+            return true;
+        else
+            return false;
+	}
+	
+	public function ordersSelectAll()
+	{
+		$query = "SELECT * FROM orders";
+		$res = $this->mysql->select($query);
+		return $res;
+	}
+	
+	public function getOrderById($id)
+	{
+		$query = "SELECT * FROM orders WHERE id= '$id'";
+		$res = $this->mysql->select($query);
+		return $res;
+	}
+	
+	public function getOrderByUser()
 }
 ?>
